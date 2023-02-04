2bf847d57438bc993409556804d6bbf688c8f4d1
cdraheim <31325092+cdraheim@users.noreply.github.com>
2023-02-04 18:43:46 -0500

    Added manual camera controls script
    Added EditorCam script to control camera manually

---
+++ Assets/Scripts/Camera Scripts/EditorCam.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorCam : MonoBehaviour {
+    
+    public static KeyCode CONFIRM_KEY = KeyCode.Mouse0;
+    public static KeyCode PAN_KEY = KeyCode.LeftAlt;
+    public static KeyCode ROTATE_KEY = KeyCode.Mouse1;
+    public static KeyCode CAMERA_KEY = KeyCode.Mouse2;
+    public static string CAMERA_INPUT_X_AXIS = "Mouse X";
+    public static string CAMERA_INPUT_Y_AXIS = "Mouse Y";
+
+    float cameraSpeed = 30f;
+    float turnSpeed = 4f;
+    float zoomSpeed = 200f;
+    float orthographicZoomSpeed = 1f;
+    public bool freeRotation = true;
+
+    // Use this for initialization
+    void Start() {
+        if(!GetComponent<Rigidbody>()) {
+            gameObject.AddComponent<Rigidbody>();
+            GetComponent<Rigidbody>().useGravity = false;
+        }
+
+        Camera.main.transform.position = GameVariables.DEFAULT_CAMERA_POSITION;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float hInput = -Input.GetAxis(CAMERA_INPUT_X_AXIS);
+        float vInput = -Input.GetAxis(CAMERA_INPUT_Y_AXIS);
+
+        if (Input.GetKey(CAMERA_KEY)) {
+            // Pan camera
+            Camera.main.GetComponent<Rigidbody>().velocity = new Vector3(hInput * cameraSpeed, vInput * cameraSpeed, 0f);
+        } else if (freeRotation && Input.GetKey(ROTATE_KEY)) {
+            // Rotate camera
+            Vector3 pos = Camera.main.GetComponent<Rigidbody>().rotation.eulerAngles;
+            Vector3 pos2 = pos;
+            pos.x += (Mathf.Abs(vInput) > .2f) ? turnSpeed * Mathf.Sign(vInput) : 0f;
+            pos.y -= (Mathf.Abs(hInput) > .2f) ? turnSpeed * Mathf.Sign(hInput) : 0f;
+            Camera.main.GetComponent<Rigidbody>().rotation = Quaternion.Slerp(Quaternion.Euler(pos2), Quaternion.Euler(pos), turnSpeed);
+        } else if (Input.GetAxis("Mouse ScrollWheel") != 0) {
+            // Zoom camera
+
+            if(Camera.main.GetComponent<Camera>().orthographic) {
+
+                Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * orthographicZoomSpeed;
+            } else {
+                Camera.main.GetComponent<Rigidbody>().velocity = Camera.main.transform.forward * Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+            }
+
+        } else if(Input.GetKey(PAN_KEY)) {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
+                // Pan camera
+                Camera.main.GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal") * cameraSpeed, Input.GetAxis("Vertical") * cameraSpeed);
+            }
+        } else {
+            // Stop camera movement
+            Camera.main.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/General/GameVariables.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameVariables {
+
+    // Tile info
+	/*
+    public static readonly Vector3 DEFAULT_TILE_SIZE = new Vector3(1f, 1f, 0.2f);
+    public static readonly Vector3 DEFAULT_2D_TILE_SIZE = new Vector3(1f, 1f);
+    public static readonly Vector3 DEFAULT_TILE_SPACING = new Vector3(0.1f, 0.1f, 0.1f);
+    public static readonly Vector3 DEFAULT_MAP_ORIGIN = new Vector3();
+    public static readonly Vector3 DEFAULT_GRID_COORDINATE_ORIGIN = new Vector3();
+    public static readonly Vector3 DEFAULT_GRID_DIMENSIONS = new Vector3(50f, 50f);
+    public static readonly VectorComponent.Constraints DEFAULT_GRID_DIMENSIONS_CONSTRAINTS = new VectorComponent.Constraints(10f, 7000f);
+	*/
+
+    //Camera info
+    public static readonly Vector3 DEFAULT_CAMERA_POSITION = new Vector3(0f, 0f, -15f);
+    public static readonly Vector3 DEFAULT_CAMERA_ROTATION = new Vector3(0f, 0f, 0f);
+}
